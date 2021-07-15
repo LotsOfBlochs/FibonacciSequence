a8009a8ebffca639d087c90e455f36ec651828a5
LotsOfBlochs <69881693+LotsOfBlochs@users.noreply.github.com>
2021-07-14 17:48:53 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FibSequence
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ulong c = 1;
+			ulong n = 0;
+			ulong o = 0;
+
+			while (0 == 0)
+			{
+				Console.WriteLine(c);
+				n = c + o;
+				if (n < c) break;
+				o = c;
+				c = n; 
+			}
+		}
+	}
+}
